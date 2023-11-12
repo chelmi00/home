92c3f275271b1f1be0d7c89a5d12c53528136471
chelmi00 <migfervil@hotmail.es>
2023-11-12 03:18:03 +0100

    A new block of conky added

--- .config/conky/conky_main.sh
+++ .config/conky/conky_main.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-conky -c /usr/share/conky/conky_maia &&
+conky -c ~/.config/conky/conky_maia &&
+conky -c ~/.config/conky/conky1.10_shortcuts_maia
 # conky -c /usr/share/conky/conky_green &&
 # conky -c /usr/share/conky/conky_grey &&
 
