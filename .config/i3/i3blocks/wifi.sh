4395a95b2ecb6fcd52267b1b20ec2056e0de548c
Miguel Fernández <80181207+chelmi00@users.noreply.github.com>
2024-09-13 16:31:46 +0200

    Delete .config/i3 directory

--- .config/i3/i3blocks/wifi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Get the name of the connected WiFi network
-WIFI_NAME=$(nmcli device wifi list | grep '*' | awk '{print $3}')
-
-# Output the name of the connected WiFi network
-echo $WIFI_NAME
-
