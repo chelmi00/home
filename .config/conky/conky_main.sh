fdce2746604279c74d8d1ae156e7d90148a2c6d2
chelmi00 <migfervil@hotmail.es>
2024-10-15 21:07:01 +0200

    Deleted files

--- .config/conky/conky_main.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-conky -c ~/.config/conky/conky_maia &&
-conky -c ~/.config/conky/conky1.10_shortcuts_maia
-# conky -c /usr/share/conky/conky_green &&
-# conky -c /usr/share/conky/conky_grey &&
-
-exit 0
